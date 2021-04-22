c07052fffa8f27649083016b6fa862dcc25511a7
YoungDon Choi <yc5ef@virginia.edu>
2021-04-22 01:19:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='pyrhessys',
+      description='A python wrapper for RHESSys',
+      url='https://github.com/UW-Hydro/pysumma.git',
+      author='YoungDon Choi',
+      author_email='choiyd1115@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          ],
+       extras_require={'requirment': [
+          'requirments.txt'
+          ],},
+      include_package_data=True)
