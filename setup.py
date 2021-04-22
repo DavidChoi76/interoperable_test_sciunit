e6c3a27da760a37c9f7da2012d5046cb52c5b018
YoungDon Choi <yc5ef@virginia.edu>
2021-04-22 02:29:32 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+from pip.__main__ import _main as main
+
+error_log = open('error_log.txt', 'w')
+
+def install(package):
+    try:
+        main(['install'] + [str(package)])
+    except Exception as e:
+        error_log.write(str(e))
+
+if __name__ == '__main__':
+    f = open('requirements.txt', 'r')
+    for line in f:
+        install(line)
+    f.close()
+    error_log.close()
+    
+setup(name='pyrhessys',
+      description='A python wrapper for SUMMA',
+      url='https://github.com/DavidChoi76/pyrhessys',
+      author='YoungDon Choi',
+      author_email='choiyd1115@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          ],
+       extras_require={'plotting': [
+          ],},
+      include_package_data=True,
+      test_suite='pyrhessys.tests')
